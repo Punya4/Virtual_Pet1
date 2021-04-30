7d938e222fe4fe9f7ba88e8b35432edbc35f5172
Punya4 <76045126+Punya4@users.noreply.github.com>
2021-04-30 17:17:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var food;
+var dog,sadDog,happyDog,database;
+function preload()
+{
+	//load images here
+  sadDog=loadImage("images/dogImg.png")
+  happyDog=loadImage("images/dogImg1.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+  database=firebase.database();
+  dog=createSprite(400,500,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.45;
+  database.ref('food').on("value",readstock);
+
+  
+}
+
+
+function draw() {  
+  background("green")
+if(keyDown("UP")){
+  writestock(food)
+  dog.addImage(happyDog);
+}
+  drawSprites();
+  textSize(25)
+  stroke("black")
+  fill("red")
+ text("PRESS UP_ARROW KEY TO FEED DRAGO MILK", 120,100)
+
+}
+
+function readstock(data){
+  food=data.val();
+}
+function writestock(x) {
+  if(x<=0){
+    x=0;
+  }
+  else {
+    x=x-1
+  }
+  database.ref('/').update({
+    food:x
+  })
+}
+
